50e44c7bde9ba3653ff454d69f57634a9d8090d1
anikethpa-stack <anikethpa411@gmail.com>
2026-07-12 14:29:13 +0530

    others can access this using the same ip and port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['43.205.152.125.nip.io'],
+    allowedHosts: ['43.205.152.125.nip.io', '43.205.152.125'],
   },
 })
 
